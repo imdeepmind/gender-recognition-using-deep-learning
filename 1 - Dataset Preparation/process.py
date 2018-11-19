a5ac78c5abf76feca6a428f93e0ab97869963fda
imdeepmind <abhishekchatterjeejit@gmail.com>
2018-11-19 19:39:03 +0530

    Updated the dataset with new images

--- 1 - Dataset Preparation/process.py
+++ 1 - Dataset Preparation/process.py
@@ -3,12 +3,13 @@ import sys
 
 def process_images(gender, iter):
     index = 1
+    counter = 0
     for i in range(1,iter):
         try:    
             path = 'rawData/' + gender + '/' + gender + '_' + str(i) + '.jpg'
             image = cv2.imread(path, 0)
             face_cascade = cv2.CascadeClassifier('../cascades/haarcascade_frontalface_alt2.xml')
-            face = face_cascade.detectMultiScale(image, 1.1, 5)
+            face = face_cascade.detectMultiScale(image, 1.1, 3)
             for x,y,w,h in face:
                 roi = image[y:y+h,x:x+w]
                 newPath = 'processedData/' + gender + '/' + gender + '_' + str(index) + '.jpg'
@@ -18,14 +19,17 @@ def process_images(gender, iter):
                 else:
                     print('-- Failed to saved the ' + gender + ' image ' + str(i) + ' --')
                 index += 1
+                counter+=1
         except Exception as e:
             print('-- Something went wrong with the image number ' + str(i) + ' --')
             print(e)
+
+    print("Total " + str(counter) + " images recovered" ) 
             
 
 
 def main():
-    no_of_images = 1600
+    no_of_images = 1500
     process_images('girls', no_of_images)
     # process_images('boys', no_of_images)
 
