a609112258b717846cf9a14dc4d7254201e6b23f
imdeepmind <abhishekchatterjeejit@gmail.com>
2018-12-23 17:58:23 +0530

    updated prototype

--- 2 - The Model/prototype.py
+++ 2 - The Model/prototype.py
@@ -5,8 +5,9 @@ from sklearn.model_selection import train_test_split
 
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPool2D, Dense, Dropout, Flatten
+from keras.preprocessing.image import ImageDataGenerator
 
-data = pd.read_csv('faces.csv')
+data = pd.read_csv('dataset/faces.csv')
 
 X_data = data.drop(['class'], axis=1)
 y_data = data['class']
@@ -18,7 +19,7 @@ X = X / 255.0
 
 model = Sequential()
 
-model.add(Conv2D(filters=32, kernel_size=(5,5), padding='same', activation='relu'))
+model.add(Conv2D(input_shape = (32,32,1), filters=32, kernel_size=(5,5), padding='same', activation='relu'))
 model.add(Conv2D(filters=32, kernel_size=(5,5), padding='same', activation='relu'))
 model.add(MaxPool2D(pool_size=(2,2)))
 model.add(Dropout(0.25))
@@ -34,13 +35,27 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.25))
 model.add(Dense(2, activation='softmax'))
 
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=2)
+
+datagen = ImageDataGenerator(
+        featurewise_center=False,
+        samplewise_center=False,  
+        featurewise_std_normalization=False,
+        samplewise_std_normalization=False,
+        zca_whitening=False,
+        rotation_range=10,  
+        zoom_range = 0.1, 
+        width_shift_range=0.1,  
+        height_shift_range=0.1,
+        horizontal_flip=True,  
+        vertical_flip=False)  
+
+
+
+datagen.fit(X_train)
+
 model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-model.fit(X, y, validation_split = 0.2, epochs=25, batch_size=64)
-
-# loss: 0.2859 - acc: 0.8833 - val_loss: 0.4149 - val_acc: 0.8025
-# loss: 0.2754 - acc: 0.8821 - val_loss: 0.3682 - val_acc: 0.8380
-# loss: 0.3097 - acc: 0.8706 - val_loss: 0.3533 - val_acc: 0.8354
-# loss: 0.0287 - acc: 0.9924 - val_loss: 0.8390 - val_acc: 0.8177
-# loss: 0.0905 - acc: 0.9645 - val_loss: 0.7569 - val_acc: 0.8253
-# loss: 0.2527 - acc: 0.8871 - val_loss: 0.4390 - val_acc: 0.7949
+model.fit_generator(datagen.flow(X_train, y_train, batch_size=32),
+                    validation_data = (X_test,y_test),
+                    steps_per_epoch=len(X_train) / 32, epochs=25)
