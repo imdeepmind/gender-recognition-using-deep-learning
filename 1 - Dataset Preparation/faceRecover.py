40d2e338594372c988b55e7819d66922c70a4db2
imdeepmind <abhishekchatterjeejit@gmail.com>
2018-12-25 23:43:09 +0530

    added more data

--- 1 - Dataset Preparation/faceRecover.py
+++ 1 - Dataset Preparation/faceRecover.py
@@ -1,5 +1,6 @@
 import cv2
 import sys
+import os
 
 def process_images(gender, iter):
     index = 1
@@ -10,16 +11,22 @@ def process_images(gender, iter):
             image = cv2.imread(path, 0)
             face_cascade = cv2.CascadeClassifier('../cascades/haarcascade_frontalface_alt2.xml')
             face = face_cascade.detectMultiScale(image, 1.1, 3)
-            for x,y,w,h in face:
-                roi = image[y:y+h,x:x+w]
-                newPath = 'processedData/' + gender + '/' + gender + '_' + str(index) + '.jpg'
-                resized_image = cv2.resize(roi, (64, 64)) 
-                if cv2.imwrite(newPath, resized_image):
-                    print('-- Sucessfully saved the ' + gender + ' image ' + str(i) + ' --')
-                else:
-                    print('-- Failed to saved the ' + gender + ' image ' + str(i) + ' --')
-                index += 1
-                counter+=1
+            if len(face) > 0:
+                for x,y,w,h in face:
+                    roi = image[y:y+h,x:x+w]
+                    newPath = 'processedData/' + gender + '/' + gender + '_' + str(index) + '.jpg'
+                    resized_image = cv2.resize(roi, (64, 64)) 
+
+                    if cv2.imwrite(newPath, resized_image):
+                        print('-- Sucessfully saved the ' + gender + ' image ' + str(i) + ' --')
+                    else:
+                        print('-- Failed to saved the ' + gender + ' image ' + str(i) + ' --')
+
+                    index += 1
+                    counter+=1
+            else:
+                os.remove(path)
+                print('-- Sucessfully deleted the ' + gender + ' image ' + str(i) + ' --')
         except Exception as e:
             print('-- Something went wrong with the image number ' + str(i) + ' --')
             print(e)
@@ -29,9 +36,9 @@ def process_images(gender, iter):
 
 
 def main():
-    no_of_images = 1500
+    no_of_images = 1800
     process_images('girls', no_of_images)
-    process_images('boys', no_of_images)
+    # process_images('boys', no_of_images)
 
 if __name__ == "__main__":
     main()
